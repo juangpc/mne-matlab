f18a6ef887f5be3bfe0adba02cf41e49c84350d1
juangpc <juangpc@gmail.com>
2019-09-18 10:12:01 -0500

    Create mne_anonymize function. Initial commit

---
+++ matlab/mne_anonymize.m
@@ -0,0 +1,29 @@
+function exitCode = mne_anonymize(inFile,varargin)
+% MNE_ANONYMIZE Anonymize fiff files
+%   MNE_ANONYMIZE('filename.fif') substitutes relevant patient and project
+%   information from the file 'filename.fif'.
+%   
+%   Dependencies: MNE Matlab
+%   (https://www.nmr.mgh.harvard.edu/mne/stable/manual/matlab.html)
+%
+%   Options:
+%
+%   inFile        example_filename.fif
+%                 Required field. The file to be anonymized.
+%
+%   verbose       [true,flase]        Default: false
+%
+%   ...
+%
+%
+%   Author: Juan Garcia-Prieto, McGovern School of Medicine (UTHealth)
+%           juangpc@gmail.com
+%   
+%   License: BSD 3-clause 
+%
+%
+
+
+
+
+
